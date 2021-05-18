b4c528fc4a0c490636b67ec5fa9ce5ba887fe508
Danjuma Nasiru Ojochide <41526722+danjumanas@users.noreply.github.com>
2021-05-19 00:26:30 +0100

    Update index.js
    added my YuGiOh function

--- index.js
+++ index.js
@@ -14,3 +14,38 @@ answer = `${n} is not a valid number but a/an ${type}`;
 }
 return answer;
 }
+
+
+function checkYuGiHo(n){
+let myArray = [1];
+let arrayItem;
+let output;
+let number = parseInt(n, 10);
+if (isNaN(number)){
+output = `invalid parameter: "${n}"`;
+} else {
+for(i=2; i <= number; i++){
+if (i%2===0 && i%3!==0 && i%5!==0){
+arrayItem = "Yu";
+} else if (i%2!==0 && i%3===0 && i%5!==0){
+arrayItem = "Gi";
+} else if (i%2!==0 && i%3!==0 && i%5===0){
+arrayItem = "Oh";
+} else if (i%2===0 && i%3===0 && i%5!==0){
+arrayItem = "Yu-Gi";
+} else if (i%2===0 && i%3!==0 && i%5===0){
+arrayItem = "Yu-Oh";
+} else if (i%2!==0 && i%3===0 && i%5===0){
+arrayItem = "Gi-Oh";
+} else if (i%2===0 && i%3===0 && i%5===0){
+arrayItem = "Yu-Gi-Oh";
+} else{
+arrayItem = i;
+}
+myArray.push(arrayItem);
+}
+console.log(myArray);
+output = myArray;
+}
+return output;
+}
