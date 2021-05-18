4fcfed9d8f16a6f741a2463ac2ed7f6ce738b778
Danjuma Nasiru Ojochide <41526722+danjumanas@users.noreply.github.com>
2021-05-18 13:00:31 +0100

    Update index.js
    fixing the array type problem with Array.isArray(n)

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-function changeFahrToCelsius (n){
+function changeFahrToCelsius(n){
 let answer;
-let parameterType = typeof n;
-let errorMsg = `${n} is not a valid number but a/an ${parameterType}`;
+let type = typeof n;
+if (Array.isArray(n)){
+answer = `${n} is not a valid number but a/an array`;
+} else {
 let number = parseInt(n, 10);
 if(!isNaN(number)){
-answer = (number-32)*(5/9);
-  answer = answer.toFixed(4);
-} else{
-answer = errorMsg;
+answer = ((number-32)*(5/9));
+answer = answer.toFixed(4);
+} else {
+answer = `${n} is not a valid number but a/an ${type}`;
+}
 }
 return answer;
 }
